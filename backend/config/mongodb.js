e39b9ba685b336f33d56a3c697ba05bb4b74b05e
Dipesh Dodke <dipeshdodke15@gmail.com>
2025-04-03 22:56:23 +0530

    databse change

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv"
 
+dotenv.config();
 const connectdb = async () => {
   mongoose.connection.on("connected", () => {
     console.log("DB Connected");
   });
   await mongoose.connect(
-    `${process.env.MONGODB_URI}/Avengers-Assemble-Network`,
+    `${process.env.MONGODB_URI}`,
     { serverSelectionTimeoutMS: 5000 }
   );
 };
